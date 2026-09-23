50959e6370af2c614c6e786d2442121c912f8db3
Guto <gutokardoso@gmail.com>
2026-09-23 19:50:09 -0300

    Add files via upload

---
+++ public/sw-v149.js
@@ -0,0 +1,5 @@
+const CACHE='ihviajei-v149';
+const CORE=['/','/style-v149.css','/app-v149.js','/manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;if(e.request.mode==='navigate'||u.pathname==='/'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('/')));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r})))});
